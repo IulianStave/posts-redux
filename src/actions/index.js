a0d71dfc81ff22900137412e190b13e8834f28dd
iulianstave <iulian.staverescu@gmail.com>
2020-03-03 15:12:44 +0200

    Add usersReducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,6 +14,12 @@ export const fetchPosts = () => {
     //   type: 'FETCH_POSTS',
     //   payload: response
     // };
-    dispatch({type: 'FETCH_POSTS', payload: response.data})
+    dispatch({type: 'FETCH_POSTS', payload: response.data});
   };
+};
+
+export const fetchUser = (id) => async dispatch => {
+  // const response = await jsonPlaceholder.get('/users'+id);
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data});
 };
